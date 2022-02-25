08c2affc04aa8324e3fd991d8eabd9fb601121cf
Made Manik Datu Yasa <manikyasa@gmail.com>
2022-02-26 02:30:24 +0700

    update content
    update 26feb2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,20 @@ $(function () {
   $(document).ready(function () {
     $(".venobox").venobox();
   });
+
+  // Wow JS
+  new WOW().init();
+
+  // Tiny Slider
+  var slider = tns({
+    container: ".testimonial-active",
+    items: 1,
+    slideBy: "page",
+    autoplay: false,
+    nav: false,
+    controlsText: [
+      '<i class= "fas fa-angle-left prev"></i>',
+      '<i class= "fas fa-angle-right next"></i>',
+    ],
+  });
 });
